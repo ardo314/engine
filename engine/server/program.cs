34c173d89edaf08c8794786fd92c7ac74be3d3e4
Leonard Hansing <lhansing92@gmail.com>
2026-01-14 17:07:42 +0000

    add nats client

--- engine/server/program.cs
+++ engine/server/program.cs
@@ -1,6 +1,25 @@
-﻿using Server;
+﻿using NATS.Net;
 
-var foo = new Foo();
-Console.WriteLine(foo.GetMessage());
-await Task.Delay(2000);
-Console.WriteLine("Server is running...");
+
+await using var natsClient = new NatsClient(url: "nats://192.168.178.33:4222");
+
+_ = Task.Run(async () =>
+{
+    while (true)
+    {
+        // Generate a random exchange rate from 1.00 to 2.00
+        double value = 1 + Random.Shared.NextDouble();
+
+        // Ensure it is 2 decimal places
+        value = Math.Round(value, 2);
+
+        // Publish it as GBPUSD
+        await natsClient.PublishAsync(subject: "GBPUSD", data: value);
+
+        // Output to console, then wait 1 second before sending another
+        Console.WriteLine($"Sent GBPUSD: {value} - press ENTER to exit.");
+        await Task.Delay(1000);
+    }
+});
+
+Console.ReadLine();
